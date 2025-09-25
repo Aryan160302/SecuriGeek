c975c88bf30c98433cfbcf587dafc25b25affe19
Aryan Chaturvedi <117507542+Aryan160302@users.noreply.github.com>
2025-09-26 02:08:15 +0530

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadComponent: () => import('./issues/issues-list.component').then(m => m.IssuesListComponent) },
-  { path: 'issues/:id', loadComponent: () => import('./issues/issue-detail.component').then(m => m.IssueDetailComponent) },
-];
